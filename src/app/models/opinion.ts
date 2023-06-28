7751c39da16b40a602843b8c058a976ad60eaa04
albaCarranza <alba.carranza.g@gmail.com>
2023-06-28 14:23:48 +0200

    subida de opiniones/estrellitas

--- src/app/models/opinion.ts
+++ src/app/models/opinion.ts
@@ -1,2 +1,10 @@
 export class Opinion {
+    constructor(
+        public id_user: number,
+        public user_name: string,
+        public photo: string,
+        public stars: number,
+        public comentario: string
+    ){
+    }
 }
