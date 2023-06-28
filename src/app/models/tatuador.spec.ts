a7abe4bcae4bce1736546838c04bbcaac88c14aa
Teresa de Miguel <teresa.demiguel.alberti@gmail.com>
2023-06-28 10:18:28 +0200

    tienda termianda y clase user/tatuador

---
+++ src/app/models/tatuador.spec.ts
@@ -0,0 +1,7 @@
+import { Tatuador } from './tatuador';
+
+describe('Tatuador', () => {
+  it('should create an instance', () => {
+    expect(new Tatuador()).toBeTruthy();
+  });
+});
--- src/app/pages/tienda/tienda.component.ts
+++ src/app/pages/tienda/tienda.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Prenda } from 'src/app/models/prenda';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tienda',
   templateUrl: './tienda.component.html',
   styleUrls: ['./tienda.component.css']
 })
-export class TiendaComponent implements OnInit{
-  @Input() prenda: Prenda;
+export class TiendaComponent implements OnInit {
+  public prendas: Prenda[];
+  public isTatuador: Boolean = true;
 
-  constructor(){}
+  constructor(private router: Router) {
+    this.prendas = [
+      new Prenda(1, "Skull shirt", "/assets/images/image 1.png"),
+      new Prenda(2, "Rose shirt", "/assets/images/image 2.png"),
+      new Prenda(3, "Beige shirt", "/assets/images/image 3.png"),
+      new Prenda(4, "Green shirt", "/assets/images/image 4.png")
+    ];
+  }
+  goAdd(){
+    this.router.navigate(['/add-producto'])
+  }
 
-  ngOnInit(): void{} 
+  ngOnInit(): void {}
 }
