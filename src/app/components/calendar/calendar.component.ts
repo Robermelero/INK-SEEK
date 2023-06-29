e426e7fe8c5f5c062c34f43a1d7a7f0ad1faaeac
Roberto Melero <roberto.meleros@gmail.com>
2023-06-29 13:18:30 +0200

    cambios header y calendario

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Cita } from 'src/app/models/cita';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent {
-  isTatuador = false
+  isTatuador = true
+  citasAgendadas: Cita[];
+  fecha : Date = new Date(2023, 6, 14, 9, 30, 0)
+  horaFinal : string = this.fecha.getHours()+":"+this.fecha.getMinutes()
+  obtenerCitasAgendadas() {
+    this.citasAgendadas = [
+      { id_user: 1,user_name: 'John Doe', date : this.fecha, hora: this.horaFinal},
+      { id_user: 1,user_name: 'John Doe', date: this.fecha, hora: this.horaFinal},
+      {  id_user: 1,user_name: 'Doe', date: this.fecha, hora : this.horaFinal}
+    ];
+    console.log(this.citasAgendadas)
+  }
+
+
 }
--- src/app/models/cita.ts
+++ src/app/models/cita.ts
@@ -1,2 +1,11 @@
 export class Cita {
+
+    constructor(
+        public id_user : number,
+        public user_name : string,
+        public date :Date,
+        public hora : string,
+    ){
+
+    }
 }
