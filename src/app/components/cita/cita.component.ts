b0e08c505b0a3f94d087ebce9d50a0853ab4306e
Leonardo David R.M <leodar1234@gmail.com>
2023-06-29 11:01:28 +0200

    Calendario arreglado

--- src/app/components/cita/cita.component.ts
+++ src/app/components/cita/cita.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Cita } from 'src/app/models/cita';
+import { Input } from '@angular/core';
 @Component({
   selector: 'app-cita',
   templateUrl: './cita.component.html',
   styleUrls: ['./cita.component.css']
 })
 export class CitaComponent {
+  @Input() listaDeCitas : Cita
+
+
 
 }
