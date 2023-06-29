547b84bbcdce03a3d19def61986bf866d5692432
Roberto Melero <roberto.meleros@gmail.com>
2023-06-29 13:57:51 +0200

    cambios

--- src/app/pages/add-cita/add-cita.component.ts
+++ src/app/pages/add-cita/add-cita.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-cita',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-cita.component.css']
 })
 export class AddCitaComponent {
+  constructor(private router: Router) {}
+  goCalendar(){
+    this.router.navigate(['/calendario'])
+  }
 
 }
