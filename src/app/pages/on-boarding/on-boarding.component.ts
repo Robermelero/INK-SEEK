287aa8f16970740c7789f13f3deb35cb84c70d46
picatostao <valver123@gmail.com>
2023-06-30 09:56:29 +0200

    fin onboard

--- src/app/pages/on-boarding/on-boarding.component.ts
+++ src/app/pages/on-boarding/on-boarding.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-on-boarding',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./on-boarding.component.css']
 })
 export class OnBoardingComponent implements OnInit{
+  constructor(private router: Router) {}
   images= [
     {src:"assets/onboard/onboardhome/01.png", text:""},//0
     {src:"assets/onboard/onboardhome/presenta.png", text:""},//2
@@ -43,17 +45,97 @@ export class OnBoardingComponent implements OnInit{
     {src:"assets/onboard/onboardhome/chat.png", text:""},//52
     
     {src:"assets/onboard/onboardhome/Home onboard transicion blur.png",text:""},//54
-    {src:"assets/onboard/onboardhome/burgertxt.png", text:""},//56
-    {src:"assets/onboard/onboardhome/burgertxt.png", text:""},//58
+    {src:"assets/onboard/onboardhome/burger-txt.png", text:""},//56
+    {src:"assets/onboard/onboardhome/burger-txt.png", text:""},//58
     {src:"assets/onboard/onboardhome/burger.png", text:""},//60
+
+    {src:"assets/onboard/calendario blur.png",text:""},//62
+    {src:"assets/onboard/añadir-citas-txt.png", text:""},//64
+    {src:"assets/onboard/añadir-citas-txt.png", text:""},//66
+    {src:"assets/onboard/añadir-citas-verde.png", text:""},//68
+
+    {src:"assets/onboard/calendario blur.png",text:""},//70
+    {src:"assets/onboard/calendario-txt.png", text:""},//72
+    {src:"assets/onboard/calendario-txt.png", text:""},//74
+    {src:"assets/onboard/calendario.png", text:""},//76
+
+    {src:"assets/onboard/calendario blur.png",text:""},//78
+    {src:"assets/onboard/consultar citas-txt.png", text:""},//80
+    {src:"assets/onboard/consultar citas-txt.png", text:""},//82
+    {src:"assets/onboard/consultar-citas.png", text:""},//84
+
+
+    {src:"assets/onboardv2/editar_perfil/perfil_tatuador_blur.png", text:""},//0 //86
+    {src:"assets/onboardv2/editar_perfil/perfil_tatuador_blur.png", text:""},//2n //88
+    {src:"assets/onboardv2/editar_perfil/editar_perfil_final_blur.png", text:""},//4 //90
+    {src:"assets/onboardv2/editar_perfil/editar_perfil_final_blur.png", text:""},//6 //92
+    {src:"assets/onboardv2/editar_perfil/editar_perfil_final_noblur.png", text:""},//8 //94                      //segundo 10//
+    
+    {src:"assets/onboardv2/editar_perfil/perfil_tatuador_blur.png", text:""},//10 //96
+    {src:"assets/onboardv2/editar_tienda/editar_tienda_final_blur.png", text:""},//12 //98
+    {src:"assets/onboardv2/editar_tienda/editar_tienda_final_blur.png", text:""},//14 //100
+    {src:"assets/onboardv2/editar_tienda/editar_tienda_final_noblur.png", text:""},//16 //102                         //segundo 18//
+
+    
+    {src:"assets/onboardv2/editar_perfil/perfil_tatuador_blur.png", text:""},//18 //104
+    {src:"assets/onboardv2/editar_valoracion/valoracion_blur.png", text:""},//20 //106
+    {src:"assets/onboardv2/editar_valoracion/valoracion_blur.png", text:""},//22 //108
+    {src:"assets/onboardv2/editar_valoracion/valoracion_noblur.png", text:""},//24 //110                           //segundo 26//
+    
+
+    {src:"assets/onboardv2/editar_perfil/perfil_tatuador_blur.png", text:""},//26 //112
+    {src:"assets/onboardv2/editar_galeria/galeria_blur.png", text:""},//28 //114
+    {src:"assets/onboardv2/editar_galeria/galeria_blur.png", text:""},//30 //116
+    {src:"assets/onboardv2/editar_galeria/galeria_noblur.png", text:""},//32 //118                                //segundo 34//
+
+
+    {src:"assets/onboardv2/mod_perfil/mod_perfil_iniblur.png", text:""},//34 //120
+    {src:"assets/onboardv2/mod_perfil/mod_perfil_iniblur.png", text:""},//36 //122
+    {src:"assets/onboardv2/mod_perfil/mod_perfil_blur.png", text:""},//38 //124
+    {src:"assets/onboardv2/mod_perfil/mod_perfil_blur.png", text:""},//40 //126
+    {src:"assets/onboardv2/mod_perfil/mod_perfil_noblur.png", text:""},//42 //128                                 //segundo 44//
+
+
+    {src:"assets/onboardv2/mod_perfil/mod_perfil_iniblur.png", text:""},//44 //130
+    {src:"assets/onboardv2/mod_perfil_dato/editar perfil tatuador2_blur.png", text:""},//46 //132
+    {src:"assets/onboardv2/mod_perfil_dato/editar perfil tatuador2_blur.png", text:""},//48 //134
+    {src:"assets/onboardv2/mod_perfil_dato/editar perfil tatuador2_noblur.png", text:""},//50 //136                  //segundo 52//
+
+
+    {src:"assets/onboardv2/chat/chat_main_blur.png", text:""},//52 //138
+    {src:"assets/onboardv2/chat/chat_noblur.png", text:""},//54 //140
+    {src:"assets/onboardv2/chat/chat_bocadillo.png", text:""},//56 //142
+    {src:"assets/onboardv2/chat/chat_bocadillo.png", text:""},//58 //144                                                      //segundo 60//
+
+
+    {src:"assets/onboardv2/artistas/artistas_blur.png", text:""},//62 //146
+    {src:"assets/onboardv2/artistas/artistas_blur.png", text:""},//64 //148
+    {src:"assets/onboardv2/artistas/artistas_noblur.png", text:""},//66 //150 
+    {src:"assets/onboardv2/artistas/artistas_noblur.png", text:""},//68 //152                                                      //segundo 68//
+    {src:"assets/onboard/onboardhome/01.png", text:""},
   ]; 
   currentIndex= 0
+  stopPresentation = false;
   next() {
-    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    if (!this.stopPresentation) {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      this.stop();
+    }
   }
+
+  stop() {
+    const lastImageIndex = this.images.length - 1;
+    if (this.currentIndex === lastImageIndex) {
+      this.stopPresentation = true;
+      this.router.navigateByUrl("/home")
+    }
+  }
+
+
   ngOnInit() {
     setInterval(() => {
       this.next();
-    }, 2000); // Cambia el duración que deseas entre cada transición en milisegundos
+    }, 2000); 
   }
 }
+
